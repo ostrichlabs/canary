68f0c2028a974ac6d40abddbd5e712005c4777df
Matt Rutherford <uhd@uhdsoft.com>
2020-07-04 11:27:13 -0700

    Added an errorcodes.h file for adding error codes. And maybe other error stuff like an array for mapping error codes to strings I dunno we'll see but FILE IS THERE.

---
+++ game/errorcodes.h
@@ -0,0 +1,24 @@
+/*
+==========================================
+Copyright (c) 2020 Ostrich Labs
+
+Big ol' list of error codes
+
+Using good ol' defines because enums might by type safe but also I don't care
+==========================================
+*/
+
+#ifndef ERRORCODES_H_
+#define ERRORCODES_H_
+
+// everything a-ok
+#define OST_ERROR_OK 0
+
+// Event Queue
+
+// Signal handler
+
+// Input handler
+
+
+#endif /* ERRORCODES_H_ */
