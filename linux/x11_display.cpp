09598e8153db5b958bfa202c97e501ee68dc0dd8
Matt Rutherford <uhd@localhost.localdomain>
2020-07-30 19:41:02 -0700

    Changed masks in DisplayX11, and added stdlibc++ to Linux release builds

--- linux/x11_display.cpp
+++ linux/x11_display.cpp
@@ -169,8 +169,9 @@ int ostrich::DisplayX11::InitWindow() {
     xwinattribs.colormap = m_Colormap;
     xwinattribs.border_pixel = 0;
     xwinattribs.event_mask = VisibilityChangeMask | ExposureMask |      // Window masks
+                             FocusChangeMask |
                              KeyPressMask | KeyReleaseMask |            // Keyboard masks
-                             PointerMotionMask | PointerMotionHintMask; // Mouse masks
+                             ButtonMotionMask | PointerMotionHintMask;  // Mouse masks
 
     m_GLWindow = ::XCreateWindow(m_Display, rootwindow, 0, 0, 1920, 1080, 0, visual->depth,
         InputOutput, visual->visual, CWBorderPixel | CWColormap | CWEventMask, &xwinattribs);
